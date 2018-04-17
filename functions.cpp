ca07522d12dece184851cf850e8026b0b82156e6
Unique I Batista <32313200+UniqueImage@users.noreply.github.com>
2018-04-17 16:23:36 -0400

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -13,22 +13,22 @@
 ///////////////////////////////////////////////////////////////////
 // FUNCTION DEFINITIONS GO HERE
 ///////////////////////////////////////////////////////////////////
-void make_triFreq(){
+
+void make_triFreq(std:: vector <int> triFreq){
     for(int i = 0; i < 17575; i++){
         triFreq.push_back(0);
     }
 }
-
-void frequencies(std::string filename){
+frequencies::frequencies(std::string filename, std:: vector <int> triFreq){
     std::ifstream infile;
     infile.open(filename);
-    
+
     std::string str;
     char ch;
     char newCh;
-    
-    
-    
+
+
+
     while (infile.get(ch)) {
         if(str.length() == 3){
             int expon = 2;
@@ -41,17 +41,20 @@ void frequencies(std::string filename){
             }
             str = "";
         }
-        
+
         if(ch - '\0' >= 65 && ch - '\0' <= 90){
             newCh = tolower(ch);
             str += newCh;
         }
-        
+
         if(ch - '\0' >= 97 && ch - '\0' <= 122){
             str += newCh;
         }
-        
+
     }
-    
+
     infile.close();
+    for (std::vector<int>::iterator i = triFreq.begin(); i != triFreq.end(); ++i){
+    std::cout << *i << ' ';
+  }
 }

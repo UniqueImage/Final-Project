2bbf8c615b9edeec65563480a7b3c946e6b1a33a
Unique I Batista <32313200+UniqueImage@users.noreply.github.com>
2018-04-23 10:40:36 -0400

    added Vectors for languages to private in Class

--- functions copy.h
+++ functions copy.h
@@ -13,6 +13,7 @@
 #include <fstream>
 #include <iostream>
 #include <cmath>
+typedef uint_least8_t vec_bin;
 
 ///////////////////////////////////////////////////////////////////
 // FUNCTION HEADERS GO HERE
@@ -23,6 +24,25 @@ class frequencies{
 
   private:
   std:: vector <int> triFreq;// creates the vector to be used
+  
+  //vectors for all the different testing_languages files to be used
+  std:: vector <vec_bin> triBork;
+  std:: vector <vec_bin> triCatalan;
+  std:: vector <vec_bin> triCzech;
+  std:: vector <vec_bin> triDutch;
+  std:: vector <vec_bin> triEnglish;
+  std:: vector <vec_bin> triFinnish;
+  std:: vector <vec_bin> triFrench;
+  std:: vector <vec_bin> triGerman;
+  std:: vector <vec_bin> triIceland;
+  std:: vector <vec_bin> triItalian;
+  std:: vector <vec_bin> triMaori;
+  std:: vector <vec_bin> triRomanian;
+  std:: vector <vec_bin> triSpanish;
+  std:: vector <vec_bin> triSwahili;
+  std:: vector <vec_bin> triSwedish;
+  
+  
 
   public:
 

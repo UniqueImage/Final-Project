19309367769f9dd1099a9100200329887c9f61d8
Unique I Batista <32313200+UniqueImage@users.noreply.github.com>
2018-04-27 14:57:31 -0400

    added the general outline

--- main copy.cpp
+++ main copy.cpp
@@ -21,6 +21,30 @@ int main(int argc, char *argv[]) {
     (void) argc;
 
     (void) argv;
-
+    
+    // create a vector that contains a vector <int> -> this stores the result of triFreq for
+        // each language
+    
+    //for (unsigned int i = 1; i < argc-1; i++){
+        //calculate a vector of frequencies (accessed with argv[i])
+        //push that into the 2D vector to store each triFreq vector for each languge}
+    
+    //now we have a 2D vector storing each freq vector for the languages 
+    //create--calculate the frequency vector the the test file as well to then be compared
+   
+        //double maxVal = -1000;
+        //unsigned int indexMax = -1;
+    
+    //for each vector<int> in 2D vector of language frequencies...    
+        //double x = similarities( 2DVector[i], triFreqTest Vector);
+        //if(maxVal < x){
+            //indexMax = i;
+            //maxVal = x;}
+    
+    //the language vector is @ the indexMax is the answer 
+    
+  
+        
+    
     return 0;
 }
